76a9b900c6c00a30769d7b742d7da06ea621a6dc
Douglas Gabriel da Silva Araújo <douglasgabriel30@gmail.com>
2024-08-12 19:20:05 -0300

    Create DataSpotify.py

---
+++ app/controllers/DataSpotify.py
@@ -0,0 +1,9 @@
+#!pip install spotipy
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID="2b5da941f2bd45be90335c85663ce744"
+CLIENT_SECRET="4401ec7039cd4d1396f4c74f88756e88"
+client_credentials_manager = SpotifyClientCredentials(cliente_id = CLENT_ID, client_secret=CLIENT_SECRET)
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
