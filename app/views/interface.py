36bc498900da5b71b087de4c3dec7009b4cf92d5
Geórgia Carin Martins da Silva <georgiacarin@gmail.com>
2024-08-21 17:03:17 -0300

    Update interface.py

--- app/views/interface.py
+++ app/views/interface.py
@@ -1,4 +1,5 @@
-# app/views/interface.py
+
+import io
 import tkinter as tk
 from tkinter import font as tkfont
 from app.controllers.Executa import Executa
@@ -6,21 +7,32 @@ import webbrowser
 from PIL import ImageTk, Image
 import urllib.request
 
-def display_image_from_url(url):
- 
-   try:
-      with urllib.request.urlopen(url) as u:
-         raw_data = u.read()
-   except Exception as e:
-      print(f"Error fetching image: {e}")
-      return
+def display_image_from_file(file_path, size=(100, 100)):
+    try:
+        image = Image.open(file_path)
+        image = image.resize(size, Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        return photo
+    except Exception as e:
+        print(f"Error opening image: {e}")
+        return None
 
-   try:
-      image = Image.open(io.BytesIO(raw_data))
-      photo = ImageTk.PhotoImage(image)
-   except Exception as e:
-      print(f"Error opening image: {e}")
-      return
+def display_image_from_url(url, size=(100, 100)):
+    try:
+        with urllib.request.urlopen(url) as u:
+            raw_data = u.read()
+    except Exception as e:
+        print(f"Error fetching image: {e}")
+        return None
+
+    try:
+        image = Image.open(io.BytesIO(raw_data))
+        image = image.resize(size, Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        return photo
+    except Exception as e:
+        print(f"Error opening image: {e}")
+        return None
 
 
     
@@ -42,41 +54,52 @@ def display_results(lista, type):
     y_position = 10
     for item in lista:
         if type == 'song':
-            display_text = f"Name: {item.nome}\nArtists: {', '.join([artist.nome for artist in item.artista])}\nPopularity: {item.popularidade}"
+            display_text = f"Nome: {item.nome}\nArtistas: {', '.join([artist.nome for artist in item.artista])}\nPopularidade: {item.popularidade}"
+            image = display_image_from_file('./song.png')  
         elif type == 'artist':
-            display_text = f"Name: {item.nome}\nGenres: {', '.join(item.generos)}\nPopularity: {item.popularidade}\nFollowers: {item.seguidores}\nUrlImage: {item.foto.url}"
+            display_text = f"Nome: {item.nome}\nGeneros: {', '.join(item.generos)}\nPopularidade: {item.popularidade}\nSeguidores: {item.seguidores}"
+            image = display_image_from_url(item.foto.url)
         elif type == 'album':
-            display_text = f"Name: {item.nome}\nNumber of Songs: {item.num_musicas}\nRelease Date: {item.data_lancamento}"
+            display_text = f"Nome: {item.nome}\nNúmero de Músicas: {item.num_musicas}\nData de Lançamento: {item.data_lancamento}"
+            image = display_image_from_url(item.foto.url)
         
-        
-        card_frame = tk.Frame(song_canvas, bg='#ffffff', bd=2, relief=tk.RAISED, width=800  )
-        card_frame.place( width=800,y=y_position, height=100 )
+        card_frame = tk.Frame(song_canvas, bg='#ffffff', bd=2, relief=tk.RAISED, width=800, height=150)
+        card_frame.place(width=800, y=y_position, height=150)
 
+        content_frame = tk.Frame(card_frame, bg='#ffffff')
+        content_frame.pack(anchor='w', padx=10, pady=10, side=tk.LEFT)
+
+        if image:
+            image_label = tk.Label(content_frame, image=image, bg='#ffffff')
+            image_label.image = image 
+            image_label.pack(side=tk.LEFT, padx=10)
+
+        text_frame = tk.Frame(content_frame, bg='#ffffff')
+        text_frame.pack(side=tk.LEFT, anchor='w')
         
-        tk.Label(card_frame, text=display_text, font=custom_font, bg='#ffffff' , justify=tk.LEFT).pack(anchor='w', padx=10, pady=1, side=tk.LEFT)
-        display_image_from_url(item.foto.url)
+        tk.Label(text_frame, text=display_text, font=custom_font, bg='#ffffff', justify=tk.LEFT).pack(anchor='w')
 
         if type in ['song', 'artist', 'album']:
-            link_label = tk.Label(card_frame, text="Listen on Spotify", font=custom_font, fg='#1DB954', bg='#ffffff', cursor="hand2")
-            link_label.pack(anchor='w', padx=10, pady=1, side=tk.LEFT )
+            link_label = tk.Label(text_frame, text="Ouvir no Spotify", font=custom_font, fg='#1DB954', bg='#ffffff', cursor="hand2")
+            link_label.pack(anchor='w', pady=5)
             link_label.bind("<Button-1>", lambda e, url=item.url: webbrowser.open(url))
-            
         
-        y_position += 120
+        y_position += 160  
 
     song_canvas.config(scrollregion=song_canvas.bbox("all"))
+
 
 def create_interface():
     root = tk.Tk()
     root.title("Spotify Search")
 
-    root.geometry("800x600")
+    root.geometry("800x800")
     root.configure(bg='#f0f0f0')
 
     global custom_font
     custom_font = tkfont.Font(family="Helvetica", size=12)
 
-    query_label = tk.Label(root, text="Enter search query:", font=custom_font, bg='#f0f0f0')
+    query_label = tk.Label(root, text="Faça sua Pesquisa:", font=custom_font, bg='#f0f0f0')
     query_label.pack(pady=10)
 
     global query_entry
@@ -89,11 +112,11 @@ def create_interface():
 
     search_type_frame = tk.Frame(root, bg='#f0f0f0')
     search_type_frame.pack(pady=10)
-    tk.Radiobutton(search_type_frame, text="Song", variable=search_var, value="Song", font=custom_font, bg='#f0f0f0').pack(side=tk.LEFT, padx=10)
-    tk.Radiobutton(search_type_frame, text="Artist", variable=search_var, value="Artist", font=custom_font, bg='#f0f0f0').pack(side=tk.LEFT, padx=10)
+    tk.Radiobutton(search_type_frame, text="Músicas", variable=search_var, value="Song", font=custom_font, bg='#f0f0f0').pack(side=tk.LEFT, padx=10)
+    tk.Radiobutton(search_type_frame, text="Artistas", variable=search_var, value="Artist", font=custom_font, bg='#f0f0f0').pack(side=tk.LEFT, padx=10)
     tk.Radiobutton(search_type_frame, text="Album", variable=search_var, value="Album", font=custom_font, bg='#f0f0f0').pack(side=tk.LEFT, padx=10)
 
-    search_button = tk.Button(root, text="Search", font=custom_font, command=search, bg='#4CAF50', fg='white', relief=tk.RAISED)
+    search_button = tk.Button(root, text="Pesquisar", font=custom_font, command=search, bg='#4CAF50', fg='white', relief=tk.RAISED)
     search_button.pack(pady=10)
 
     global song_canvas
